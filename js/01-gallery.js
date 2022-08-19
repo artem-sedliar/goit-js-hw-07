d26b8e059f1a638691b8d3e7e7bedf7b66b100a0
artem-sedliar <artemsedlar@gmail.com>
2022-08-19 12:39:11 +1200

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,11 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+const items = []
+galleryItems.forEach(element => {
+	const galleryItem = document.createElement('div')
+	galleryItem.className = 'gallery__item'
+	
+})
